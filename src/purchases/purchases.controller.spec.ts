b3ba03e216d897052fcd44fcf81135b81d6ef5a4
Alban GUILLET <albanguillet98@gmail.com>
2020-02-28 15:34:00 +0100

    spec: Add purchasesController findAll tests

--- src/purchases/purchases.controller.spec.ts
+++ src/purchases/purchases.controller.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Customer } from '../customers/customer.entity';
+import { PurchasesService } from './purchases.service';
 import { PurchasesController } from './purchases.controller';
+import { Purchase } from './purchase.entity';
 
-describe('Purchases Controller', () => {
-  let controller: PurchasesController;
+describe('Customers Controller', () => {
+  let purchasesController: PurchasesController;
+  let purchasesService: PurchasesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PurchasesController],
+      providers: [
+        {
+          provide: getRepositoryToken(Customer),
+          useClass: Repository
+        },
+        {
+          provide: getRepositoryToken(Purchase),
+          useClass: Repository
+        },
+        PurchasesService
+      ]
     }).compile();
 
-    controller = module.get<PurchasesController>(PurchasesController);
+    purchasesController = module.get<PurchasesController>(PurchasesController);
+    purchasesService = module.get<PurchasesService>(PurchasesService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of purchases', async () => {
+      const customerId: string = undefined;
+      const result: Purchase[] = [new Purchase(), new Purchase()];
+      jest.spyOn(purchasesService, 'findAll').mockResolvedValue(result);
+
+      expect(await purchasesController.findAll(customerId)).toBe(result);
+    });
+  });
+
+  describe('findAll', () => {
+    it("should return customers' purchases", async () => {
+      const customerId = 'd73426d4-e278-4d1a-b308-07997429c8c7';
+      const result: Purchase[] = [new Purchase(), new Purchase()];
+      jest.spyOn(purchasesService, 'findAll').mockResolvedValue(result);
+
+      expect(await purchasesController.findAll(customerId)).toBe(result);
+    });
   });
 });
