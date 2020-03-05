cad7c701de12e5d97e8fcd12f9d01824505479a4
Alban GUILLET <albanguillet98@gmail.com>
2020-03-05 15:54:37 +0100

    fix: Changing tests to follow the class-validation implementation

--- src/analytics/analytics.service.spec.ts
+++ src/analytics/analytics.service.spec.ts
@@ -1,18 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from '../products/product.entity';
+import { Purchase } from '../purchases/purchase.entity';
+import { Analytics } from './analytics.interface';
 import { AnalyticsService } from './analytics.service';
+import { GetAnalyticsParams } from './params/analytics.param';
 
-describe('AnalyticsService', () => {
-  let service: AnalyticsService;
+describe('Analytics Service', () => {
+  let analyticsService: AnalyticsService;
+  let purchaseRepository: Repository<Purchase>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AnalyticsService],
+      providers: [
+        {
+          provide: getRepositoryToken(Purchase),
+          useClass: Repository
+        },
+        AnalyticsService
+      ]
     }).compile();
 
-    service = module.get<AnalyticsService>(AnalyticsService);
+    analyticsService = module.get<AnalyticsService>(AnalyticsService);
+    purchaseRepository = module.get<Repository<Purchase>>(
+      getRepositoryToken(Purchase)
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('getAnalytics', () => {
+    it('should return analytics for the specified year', async () => {
+      const params: GetAnalyticsParams = { year: 2020 };
+      const purchase1: Purchase = new Purchase();
+      purchase1.quantity = 2;
+      purchase1.product = new Product();
+      purchase1.product.price = 2;
+      const purchase2: Purchase = new Purchase();
+      purchase2.quantity = 2;
+      purchase2.product = new Product();
+      purchase2.product.price = 2;
+      const purchases: Purchase[] = [purchase1, purchase2];
+      const result: Analytics = {
+        year: params.year,
+        turnover: 8
+      };
+      jest.spyOn(purchaseRepository, 'find').mockResolvedValue(purchases);
+
+      expect(await analyticsService.getAnalytics(params)).toStrictEqual(result);
+    });
+
+    it('should return analytics for a year with no purchases', async () => {
+      const params: GetAnalyticsParams = { year: 2019 };
+      const purchases: Purchase[] = [];
+      const result: Analytics = {
+        year: params.year,
+        turnover: 0
+      };
+      jest.spyOn(purchaseRepository, 'find').mockResolvedValue(purchases);
+
+      expect(await analyticsService.getAnalytics(params)).toStrictEqual(result);
+    });
   });
 });
