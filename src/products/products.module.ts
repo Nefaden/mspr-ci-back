a6916346176adf7ff1ea6267f1eeeeca3af4748a
Alban GUILLET <albanguillet98@gmail.com>
2020-03-24 15:45:30 +0100

    fix: Fixing e2e tests

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './product.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])]
