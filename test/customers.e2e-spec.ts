a9a7d0033c409aee01cd686ad2a26bb13003edf0
Alban GUILLET <albanguillet98@gmail.com>
2020-03-24 10:49:41 +0100

    fix: fixing customers e2e test

---
+++ test/customers.e2e-spec.ts
@@ -0,0 +1,42 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { Repository } from 'typeorm';
+import { AppModule } from '../src/app.module';
+import { Customer } from '../src/customers/customer.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+describe('Customers (e2e)', () => {
+  let app: INestApplication;
+  let customersRepository: Repository<Customer>;
+  const customer = {
+    id: '1f4b3f76-c229-4a09-9c30-d95785b29007',
+    email: 'test@test.fr',
+    firstName: 'Test',
+    lastName: 'Test'
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile();
+
+    app = module.createNestApplication();
+    await app.init();
+
+    customersRepository = module.get(getRepositoryToken(Customer));
+    customersRepository.save(customer);
+  });
+
+  it('/customers (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/customers')
+      .expect(200)
+      .expect([customer]);
+  });
+
+  afterEach(() => {
+    customersRepository.delete(customer);
+    app.close();
+  });
+});
