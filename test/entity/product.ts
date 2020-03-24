1c848d9d80d3e07c12bee968022eee70d0acdeec
Alban GUILLET <albanguillet98@gmail.com>
2020-03-24 15:55:36 +0100

    fix: Fixing e2e tests

--- test/entity/product.ts
+++ test/entity/product.ts
@@ -2,6 +2,6 @@ export const getProduct = () => {
   return {
     id: '77e5c8ac-942a-439b-b6b8-9558c6c227bd',
     name: 'Test',
-    price: 2.4
+    price: 2.400
   };
 };
--- test/entity/purchase.ts
+++ test/entity/purchase.ts
@@ -5,7 +5,7 @@ export const getPurchase = () => {
   return {
     id: 'fc2d715c-dc99-4cad-a095-b1c572ddfe55',
     quantity: 5,
-    date: '2020-03-05 17:58:12',
+    date: '2020-03-05T17:58:12.000Z',
     customer: getCustomer(),
     product: getProduct()
   };
