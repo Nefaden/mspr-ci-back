46f83650cfe18a7bb3a017b39a4326f110a9159b
Alban GUILLET <albanguillet98@gmail.com>
2020-03-24 11:02:35 +0100

    fix: fixing customers e2e test

--- test/customers.e2e-spec.ts
+++ test/customers.e2e-spec.ts
@@ -13,7 +13,7 @@ describe('Customers (e2e)', () => {
     id: '1f4b3f76-c229-4a09-9c30-d95785b29007',
     email: 'test@test.fr',
     firstName: 'Test',
-    lastName: 'Test'
+    lastName: 'TEST'
   };
 
   beforeEach(async () => {
